f1735e0967a5ede8c5f416e5628a502f4c4fef9e
Yury Murashka <Yury_Murashka@epam.com>
2016-10-05 17:58:09 +0300

    added changes

---
+++ app.js
@@ -0,0 +1,13 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
